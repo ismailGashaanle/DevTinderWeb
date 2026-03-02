6786ae115a57de8f5b00b9d680b2e33c44d51909
Ismail Ahmed Hassan <ismailgashaanle@gmail.com>
2026-03-02 09:52:08 +0300

    Use environment variable for backend URL

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,3 +1,4 @@
 //  export const Base_URL="http://localhost:5000"
-  export const Base_URL=import.meta.env.VITE_API_URL;
+//   export const Base_URL=import.meta.env.VITE_API_URL;
+export const Base_URL = import.meta.env.VITE_API_URL;
  export const default_Profile_Image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKKOdmJz8Z2pDtYgFgR2u9spABvNNPKYYtGw&s"
