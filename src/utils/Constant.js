34ab642709c289034a0eedd0723b4b0c2725b72e
Ismail Ahmed Hassan <ismailgashaanle@gmail.com>
2026-03-03 04:38:02 +0300

    update for start running

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,4 +1,5 @@
-//  export const Base_URL="http://localhost:5000"
+ export const Base_URL= location.hostname==="localhost" ? "http://localhost:5000" : "DevtinderChat"
 //   export const Base_URL=import.meta.env.VITE_API_URL;
-export const Base_URL = import.meta.env.VITE_API_URL;
+// export const Base_URL = import.meta.env.VITE_API_URL
  export const default_Profile_Image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKKOdmJz8Z2pDtYgFgR2u9spABvNNPKYYtGw&s"
+//  export const Base_URL = import.meta.env.VITE_API_URL || "http://localhost:5000"
